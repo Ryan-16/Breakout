3a47cb27d212f0c3ba22b2188ba7b1df4bd815ef
Ryan Lail <me@ryanlail.com>
2020-09-18 12:00:34 +0100

    beautify

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -1,38 +1,31 @@
 #include "Ball.h"
 
-Ball::Ball(double x, double y, int speed)
-{
+Ball::Ball(double x, double y, int speed) {
     set_x(x);
     set_y(y);
     set_speed(speed);
 }
 
-void Ball::set_x(double a)
-{
+void Ball::set_x(double a) {
     x = a;
 }
 
-void Ball::set_y(double a)
-{
+void Ball::set_y(double a) {
     y = a;
 }
-    
-void Ball::set_speed(int a)
-{
+
+void Ball::set_speed(int a) {
     speed = a;
 }
 
-double Ball::get_x()
-{ 
-	return x; 
+double Ball::get_x() {
+    return x;
 }
 
-double Ball::get_y()
-{ 
-	return y; 
+double Ball::get_y() {
+    return y;
 }
 
-int Ball::get_speed()
-{ 
-	return speed; 
+int Ball::get_speed() {
+    return speed;
 }

1b00671614138970a114dca43097aa3872145255
Ryan Lail <me@ryanlail.com>
2020-09-18 11:59:45 +0100

    beuttify paddle.cpp

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -1,61 +1,50 @@
 #include "Paddle.h"
 
-Paddle::Paddle(double x, double y, double width, int speed)
-{
-    set_x(x);
-    set_y(y);
-    // default height 1 to yield flat paddle
-    set_height(1);
-    set_width(width);
-    set_speed(speed);
+Paddle::Paddle(double x, double y, double width, int speed) {
+  set_x(x);
+  set_y(y);
+  // default height 1 to yield flat paddle
+  set_height(1);
+  set_width(width);
+  set_speed(speed);
 }
 
-void Paddle::set_x(double a)
-{
-    x = a;
+void Paddle::set_x(double a) {
+  x = a;
 }
 
-void Paddle::set_y(double a)
-{
-    y = a;
+void Paddle::set_y(double a) {
+  y = a;
 }
 
-void Paddle::set_height(double a)
-{
-	height = a;
-}
-    
-void Paddle::set_width(double a)
-{
-    width = a;
+void Paddle::set_height(double a) {
+  height = a;
 }
 
-void Paddle::set_speed(int a)
-{
-	speed = a;
+void Paddle::set_width(double a) {
+  width = a;
 }
 
-double Paddle::get_x()
-{ 
-	return x; 
+void Paddle::set_speed(int a) {
+  speed = a;
 }
 
-double Paddle::get_y()
-{ 
-	return y; 
+double Paddle::get_x() {
+  return x;
 }
 
-double Paddle::get_height()
-{ 
-	return height; 
+double Paddle::get_y() {
+  return y;
 }
 
-double Paddle::get_width()
-{ 
-	return width; 
+double Paddle::get_height() {
+  return height;
 }
 
-int Paddle::get_speed()
-{
-	return speed;
+double Paddle::get_width() {
+  return width;
+}
+
+int Paddle::get_speed() {
+  return speed;
 }
